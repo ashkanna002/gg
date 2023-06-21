478ac425c0d98a4477b5cb89b254dac1fde64af0
ashkanna002 <erfanfatemi95@gmail.com>
2023-06-21 18:13:22 +0330

    sdasdsadsadsad

---
+++ ssd.js
@@ -0,0 +1,19 @@
+let i = 0
+let students = [{
+    name: "hosein",
+    age:45
+
+}
+,
+{
+    name:"maamdli",
+    age:85
+}]
+while(i<10){
+    if (students[i].age<55){
+        i=i+1
+        console.log(i)
+    
+    }
+    
+}
